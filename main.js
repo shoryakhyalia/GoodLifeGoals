a6905915622c3bcec3ee29bab1069744c6cb861a
shoryakhyalia <154425600+shoryakhyalia@users.noreply.github.com>
2024-01-21 19:00:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function about()
+{
+       window.location ="index2.html";
+}
+function back()
+{
+       window.location ="index.html";
+}
+function other()
+{
+       window.location ="index3.html";
+}
